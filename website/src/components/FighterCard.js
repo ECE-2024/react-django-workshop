7d174dc743f8ba53b40f9a96c858ecedc8b9a2fe
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-02-14 15:59:31 -0500

    Add datetime created to fighter card

--- website/src/components/FighterCard.js
+++ website/src/components/FighterCard.js
@@ -4,7 +4,9 @@ import { Button, Card, ListGroup } from "react-bootstrap";
 function FighterCard(props) {
   return (
     <Card className="my-3 py-3 mx-auto col-3">
-      <Card.Title className="text-center"><h3>{props.fighter.name}</h3></Card.Title>
+      <Card.Title className="text-center">
+        <h3>{props.fighter.name}</h3>
+      </Card.Title>
       <Card.Img variant="top" src={props.fighter.image} />
       <Card.Body>
         <Card.Text className="text-center">
@@ -17,6 +19,7 @@ function FighterCard(props) {
           <ListGroup.Item>Type: {props.fighter.type}</ListGroup.Item>
           <ListGroup.Item>Weakness: {props.fighter.weakness}</ListGroup.Item>
         </ListGroup>
+        <div className="mt-3">Created: {props.fighter.datetime_created}</div>
       </Card.Body>
       <div>
         <Button className="col-6 bg-success">Level Up!</Button>
