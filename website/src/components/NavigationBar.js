d7457351435f7d70b8cd05cd6d79f5eed08a4675
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-02-14 15:15:29 -0500

    Fix navbar to the top and increase text size

--- website/src/components/NavigationBar.js
+++ website/src/components/NavigationBar.js
@@ -3,8 +3,10 @@ import { Navbar } from "react-bootstrap";
 
 function NavigationBar() {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand className="m-auto">Kounter Shot!</Navbar.Brand>
+    <Navbar bg="dark" variant="dark" fixed="top">
+      <Navbar.Brand className="m-auto">
+        <h4>Kounter Shot!</h4>
+      </Navbar.Brand>
     </Navbar>
   );
 }
