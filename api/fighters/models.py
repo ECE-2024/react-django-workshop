b6e4d277f4edba3d11c79836dc70ca2c0931011d
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-02-15 12:12:54 -0500

    Allow description to be nullable

--- api/fighters/models.py
+++ api/fighters/models.py
@@ -6,7 +6,7 @@ FIGHTER_TYPE_CHOICES = [(0, "Water"), (1, "Earth"), (2, "Fire"), (3, "Air")]
 class Fighter(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True)
     health = models.IntegerField()
     attack = models.IntegerField()
     speed = models.IntegerField()
