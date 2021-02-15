9f1fcda21ccf98de52dbab064e0280eaae41bab7
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-02-15 01:31:20 -0500

    Set upload directory for images to media folder

--- api/fighters/models.py
+++ api/fighters/models.py
@@ -12,7 +12,7 @@ class Fighter(models.Model):
     speed = models.IntegerField()
     fighter_type = models.IntegerField(choices=FIGHTER_TYPE_CHOICES)
     weakeness = models.IntegerField(choices=FIGHTER_TYPE_CHOICES)
-    image = models.ImageField(null=True)
+    image = models.ImageField(upload_to="media", null=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
