07f2b9ae8c0aa8a895da2d8f5a42086e5c17499b
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-02-15 01:27:34 -0500

    Create fighter view set

--- api/fighters/views.py
+++ api/fighters/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Fighter
+from .serializers import FighterSerializer
 
-# Create your views here.
+
+class FightersViewSet(ModelViewSet):
+    queryset = Fighter.objects.all()
+    serializer_class = FighterSerializer
