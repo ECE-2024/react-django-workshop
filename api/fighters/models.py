76d5a2d556752056d69a8026f7e2420655facdd5
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2021-02-15 01:04:58 -0500

    Create Fighter model

--- api/fighters/models.py
+++ api/fighters/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 
+FIGHTER_TYPE_CHOICES = [(0, "Water"), (1, "Earth"), (2, "Fire"), (3, "Air")]
 
 # Create your models here.
+class Fighter(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100)
+    health = models.IntegerField()
+    attack = models.IntegerField()
+    speed = models.IntegerField()
+    fighter_type = models.IntegerField(choices=FIGHTER_TYPE_CHOICES)
+    weakeness = models.IntegerField(choices=FIGHTER_TYPE_CHOICES)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-datetime_created"]
